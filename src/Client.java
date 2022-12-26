5bd6adbfe614317f7eedf3815c812459483e95c6
Aisha <ivoninaanastasia16@gmail.com>
2022-12-26 19:32:42 +0300

    сервер

---
+++ src/Client.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    // 2 потока помимо основного
+    //поток отправитель сообщений
+    //поток получатель сообщений
+    // driver code
+    public static void main(String[] args)
+    {
+        // establish a connection by providing host and port
+        // number
+        try (Socket socket = new Socket("localhost", 1234)) {
+
+            // writing to server
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+
+            // reading from server
+            BufferedReader in
+                    = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+
+            // object of scanner class
+            Scanner sc = new Scanner(System.in);
+            String line = null;
+
+            while (!"exit".equalsIgnoreCase(line)) {
+
+                // reading from user
+                line = sc.nextLine();
+
+                // sending the user input to server
+                out.println(line);
+                out.flush();
+
+                // displaying server reply
+                System.out.println("Server replied "
+                        + in.readLine());
+            }
+
+            // closing the scanner object
+            sc.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
